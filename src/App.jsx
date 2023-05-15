f53b1d1b929fe4d5e7eb517a2e4c4b31b6c89fae
Edilson Bispo <edilson_pixot@hotmail.com>
2023-05-15 15:14:34 -0300

    Calculo do IMC

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Calculo_imc from "./components/calculator/Imc";
+import Header from "./components/header/header";
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Calculo_imc />
+    </>
+
+  )
+}
+
+export default App
